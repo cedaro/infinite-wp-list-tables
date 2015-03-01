9addb06d28464eebd3011f48171748c09e1b9a39
Brady Vercher <brady@blazersix.com>
2015-02-28 22:58:54 -0800

    Rewrite to support comment lists and BuddyPress, among other improvements.

--- README.md
+++ README.md
@@ -2,21 +2,23 @@
 
 Infinite scroll support for WP List Tables in the WordPress admin panel.
 
-__Contributors:__ [Brady Vercher](https://twitter.com/bradyvercher)  
-__Requires:__ 4.0  
-__Tested up to:__ 4.1  
-__License:__ [GPL-2.0+](http://www.gnu.org/licenses/gpl-2.0.html)  
+__Contributors:__ [Brady Vercher](https://twitter.com/bradyvercher)
+__Requires:__ 4.0
+__Tested up to:__ 4.1
+__License:__ [GPL-2.0+](http://www.gnu.org/licenses/gpl-2.0.html)
 
-## Installation ##
+Supports list tables for posts, pages, comments, users and most custom post types.
 
-### Upload ###
+## Installation
+
+### Upload
 
 1. Download the [latest release](https://github.com/cedaro/infinite-wp-list-tables/archive/master.zip) from GitHub.
 2. Go to the _Plugins &rarr; Add New_ screen in your WordPress admin panel and click the __Upload__ button at the top next to the "Add Plugins" title.
 3. Upload the zipped archive.
 4. Click the __Activate Plugin__ link after installation completes.
 
-### Manual ###
+### Manual
 
 1. Download the [latest release](https://github.com/cedaro/infinite-wp-list-tables/archive/master.zip) from GitHub.
 2. Unzip the archive.
@@ -25,10 +27,24 @@ __License:__ [GPL-2.0+](http://www.gnu.org/licenses/gpl-2.0.html)
 
 Read the Codex for more information about [installing plugins manually](http://codex.wordpress.org/Managing_Plugins#Manual_Plugin_Installation).
 
-### Git ###
+### Git
 
 Clone this repository in `/wp-content/plugins/`:
 
 `git clone git@github.com:cedaro/infinite-wp-list-tables.git`
 
 Then go to the _Plugins &rarr; Installed Plugins_ screen in your WordPress admin panel and click the __Activate__ link under the _Infinite WP List Tables_ item.
+
+## Changelog
+
+### 2.0.0
+
+* Rewrote the plugin to add support for the comment list table and BuddyPress.
+* Added a filter to make it easier for plugins with custom list tables to add integration.
+* Added support for loading translation files.
+* Bundled the unminified version of jquery.infinitescroll.js for use when `SCRIPT_DEBUG` is enabled.
+* Ensure the pagination links aren't hidden to more easily navigate many pages.
+
+### 1.0.0
+
+* Initial release.
--- infinite-wp-list-tables.php
+++ infinite-wp-list-tables.php
@@ -12,7 +12,7 @@
  * Plugin Name:       Infinite WP List Tables
  * Plugin URI:        https://github.com/cedaro/infinite-wp-list-tables
  * Description:       Infinite scroll support for WP List Tables in the WordPress admin panel.
- * Version:           1.0.0
+ * Version:           2.0.0
  * Author:            Cedaro
  * Author URI:        http://www.cedaro.com/
  * License:           GPL-2.0+
@@ -23,74 +23,179 @@
  */
 
 /**
- * Localize the plugin's strings.
+ * Main plugin class.
  *
- * @since 1.0.1
+ * @package InfiniteWPListTables
+ * @since 2.0.0
  */
-function cedaro_infinite_wp_list_tables_load_textdomain() {
-	$plugin_rel_path = dirname( plugin_basename( __FILE__ ) ) . '/languages';
-	load_plugin_textdomain( 'infinite-wp-list-tables', false, $plugin_rel_path );
+class Cedaro_Infinite_WP_List_Tables {
+	/**
+	 * Load the plugin.
+	 *
+	 * @since 2.0.0
+	 */
+	public function load() {
+		$this->load_textdomain();
+		$this->register_hooks();
+	}
+
+	/**
+	 * Localize the plugin's strings.
+	 *
+	 * @since 2.0.0
+	 */
+	public function load_textdomain() {
+		$plugin_rel_path = dirname( plugin_basename( __FILE__ ) ) . '/languages';
+		load_plugin_textdomain( 'infinite-wp-list-tables', false, $plugin_rel_path );
+	}
+
+	/**
+	 * Register hooks.
+	 *
+	 * @since 2.0.0
+	 */
+	public function register_hooks() {
+		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_assets' ) );
+		add_action( 'admin_head', array( $this, 'print_css' ) );
+		add_action( 'admin_print_footer_scripts', array( $this, 'print_script' ) );
+	}
+
+	/**
+	 * Enqueue and print assets to make WP List Tables support infinite scroll.
+	 *
+	 * @since 2.0.0
+	 */
+	public function enqueue_assets( $hook_suffix ) {
+		$suffix = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
+
+		wp_enqueue_script(
+			'jquery-infinite-scroll',
+			plugin_dir_url( __FILE__ ) . 'assets/js/vendor/jquery.infinitescroll' . $suffix . '.js',
+			array( 'jquery' ),
+			'2.1.0',
+			true
+		);
+
+		wp_localize_script( 'jquery-infinite-scroll', '_iwpltSettings', array(
+			'l10n'      => array(
+				'loadingMessage' => __( 'Loading&hellip;', 'infinite-wp-list-tables' ),
+			),
+			'selectors' => $this->get_selectors(),
+		) );
+	}
+
+	/**
+	 * Print CSS to support the Infinite Scroll script.
+	 *
+	 * @since 2.0.0
+	 */
+	public function print_css() {
+		?>
+		<style type="text/css">
+		tr#iwplt-loading td {
+			padding: 10px;
+			vertical-align: middle;
+		}
+
+		tr#iwplt-loading .spinner {
+			display: inline-block;
+			float: none;
+			margin-top: 0;
+			vertical-align: middle;
+		}
+		</style>
+		<?php
+	}
+
+	/**
+	 * Print the script to initialize Infinite Scroll.
+	 *
+	 * @since 2.0.0
+	 */
+	public function print_script() {
+		?>
+		<script type="text/javascript">
+		jQuery(function( $ ) {
+			var settings = _iwpltSettings,
+				$msg = $( '<tr id="iwplt-loading"><td class="colspanchange"></td></tr>' ),
+				$table = $( settings.selectors.table );
+
+			// Ensure the loading message cell spans the appropriate number of columns.
+			$msg.find( '.colspanchange' )
+				.attr( 'colspan', $table.find( 'thead tr' ).children( ':visible' ).length )
+				.append( '<span class="spinner" /> ' )
+				.append( settings.l10n.loadingMessage );
+
+			$table.find( settings.selectors.content ).infinitescroll({
+				loading: {
+					finished: function() {
+						$( '#iwplt-loading' ).hide();
+					},
+					msg: $msg
+				},
+				navSelector: '.pagination-links',
+				nextSelector: '.next-page',
+				itemSelector: settings.selectors.item,
+				contentSelector: settings.selectors.content,
+				maxPage: $( '.total-pages' ).first().text()
+			}, function() {
+				// Keep the pagination links visible.
+				$( '.pagination-links' ).show();
+			});
+		});
+		</script>
+		<?php
+	}
+
+	/**
+	 * Retrieve the selectors for the current screen.
+	 *
+	 * @since 2.0.0
+	 *
+	 * @return array
+	 */
+	protected function get_selectors() {
+		$screen_id = get_current_screen()->id;
+
+		$selectors = array(
+			'content' => '#the-list',
+			'item'    => '#the-list tr',
+			'table'   => '.wp-list-table',
+		);
+
+		// Comment List Table
+		if ( 'edit-comments' == $screen_id ) {
+			$selectors = array(
+				'content' => '#the-comment-list',
+				'item'    => '#the-comment-list tr',
+				'table'   => 'table.comments',
+			);
+		}
+
+		// BuddyPress Activity List Table
+		if ( 'toplevel_page_bp-activity' == $screen_id ) {
+			$selectors = array(
+				'content' => '#the-comment-list',
+				'item'    => '#the-comment-list tr',
+				'table'   => 'table.activities',
+			);
+		}
+
+		// BuddyPress Group List Table
+		if ( 'toplevel_page_bp-groups' == $screen_id ) {
+			$selectors = array(
+				'content' => '#the-comment-list',
+				'item'    => '#the-comment-list tr',
+				'table'   => 'table.groups',
+			);
+		}
+
+		return apply_filters( 'infinite_wp_list_tables_selectors', $selectors );
+	}
 }
-add_action( 'plugins_loaded', 'cedaro_infinite_wp_list_tables_load_textdomain' );
 
 /**
- * Enqueue and print assets to make WP List Tables support infinite scroll.
- *
- * @since 1.0.0
+ * Initialize the plugin.
  */
-function cedaro_infinite_wp_list_tables() {
-	wp_enqueue_script(
-		'jquery-infinite-scroll',
-		plugin_dir_url( __FILE__ ) . 'assets/js/vendor/jquery.infinitescroll.min.js',
-		array( 'jquery' ),
-		'2.1.0',
-		true
-	);
-
-	wp_localize_script( 'jquery-infinite-scroll', 'iwpltL10n', array(
-		'loadingMessage' => __( 'Loading&hellip;', 'infinite-wp-list-tables' ),
-	) );
-	?>
-
-	<script type="text/javascript">
-	jQuery(function( $ ) {
-		var $msg = $( '<tr id="iwplt-loading"><td class="colspanchange"></td></tr>' );
-
-		// Ensure the loading message cell spans the appropriate number of columns.
-		$msg.find( '.colspanchange' )
-			.attr( 'colspan', $( '.wp-list-table thead tr' ).children( ':visible' ).length )
-			.append( '<span class="spinner"></span> ' )
-			.append( iwpltL10n.loadingMessage );
-
-		$( '#the-list' ).infinitescroll({
-			loading: {
-				finished: function() {
-					$( '#iwplt-loading' ).hide();
-				},
-				msg: $msg
-			},
-			navSelector: '.pagination-links',
-			nextSelector: '.next-page',
-			itemSelector: '#the-list tr',
-			contentSelector: '#the-list',
-			maxPage: $( '.total-pages' ).first().text()
-		});
-	});
-	</script>
-
-	<style type="text/css">
-	tr#iwplt-loading td {
-		padding: 10px;
-		vertical-align: middle;
-	}
-
-	tr#iwplt-loading .spinner {
-		display: inline-block;
-		float: none;
-		margin-top: 0;
-		vertical-align: middle;
-	}
-	</style>
-	<?php
-}
-add_action( 'admin_footer', 'cedaro_infinite_wp_list_tables' );
+$infinite_wp_list_tables = new Cedaro_Infinite_WP_List_Tables();
+add_action( 'plugins_loaded', array( $infinite_wp_list_tables, 'load' ) );
